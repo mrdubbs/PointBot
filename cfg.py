0325d6c5409ebadc9fcb682b18fd66fc8ba9e18c
mrdubbs <tofealimi@hotmail.com>
2017-01-01 00:26:22 -0800

    moved macros to cfg.py

--- cfg.py
+++ cfg.py
@@ -1,3 +1,13 @@
-# macros used in bot main program
-CHANNEL = 'armadaugs'.lower()
-PPM = 1
+import mysql.connector as sql
+
+# macros used in pointbot/botmain
+HOST = 'irc.chat.twitch.tv'
+PORT = 6667
+NICK = 'dubbsbot'
+PASS = '***'
+TWITCH = 'https://api.twitch.tv/kraken'
+HOME = 'mrdubbs'
+CLIENT_ID = '***'
+IO = 'iofiles/'
+CONN = sql.connect(host='localhost', database = 'sys', user='root', password='***')
+CURSOR = CONN.cursor(dictionary=True)
